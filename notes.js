0a98c50998b4b2372f00632a566dfe8ddb0f5613
Umarahmad03 <91283374+Umarahmad03@users.noreply.github.com>
2022-09-03 18:35:58 -0500

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,29 @@
+const notes = [
+    {
+      key: 1,
+      title: "Language Used",
+      content:
+        "I used HTML CSS and JavaScript along with React to create this project"
+    },
+    {
+      key: 2,
+      title: "Components",
+      content:
+        "I used components to break down this project into a Footer, Header, Note, and App section in order to make it cleaner to read"
+    },
+    {
+      key: 3,
+      title: "Props",
+      content:
+        "I also used props as a means of passing data from one component to another"
+    },
+    {
+      key: 4,
+      title: "GitHub",
+      content:
+        "On top of all of this, I also got an idea on how to use GitHub"
+    }
+  ];
+  
+  export default notes;
+  
